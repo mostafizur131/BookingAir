b286d29f5b0f95a26bd548bac4495a12b7343e75
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-12-25 16:20:49 +0600

    create user for sing up implemented

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAWiFL4-Prh6ppzr5ElLYeGet4Nlg2c6wQ",
   authDomain: "bookingair-240b8.firebaseapp.com",
@@ -13,5 +9,6 @@ const firebaseConfig = {
   appId: "1:438142852386:web:7b0ec39001c7ae1a09597b",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
