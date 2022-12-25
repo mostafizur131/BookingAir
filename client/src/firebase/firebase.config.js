6e47983a7f781e73d330ab29dec8cc0baceec95d
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-12-25 14:40:48 +0600

    started BookingAir web app creation with MERN stack

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,9 +1,17 @@
-import { initializeApp } from 'firebase/app'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // COnfig code goes here
-}
+  apiKey: "AIzaSyAWiFL4-Prh6ppzr5ElLYeGet4Nlg2c6wQ",
+  authDomain: "bookingair-240b8.firebaseapp.com",
+  projectId: "bookingair-240b8",
+  storageBucket: "bookingair-240b8.appspot.com",
+  messagingSenderId: "438142852386",
+  appId: "1:438142852386:web:7b0ec39001c7ae1a09597b",
+};
 
-const app = initializeApp(firebaseConfig)
-
-export default app
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
